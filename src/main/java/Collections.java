6cddc02c45362257c717fffab3d520e5caf51e50
Burak Köken <burakkokenn@gmail.com>
2017-07-06 21:16:43 +0200

    Initial Commit

---
+++ src/main/java/Collections.java
@@ -0,0 +1,48 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class Collections {
+	
+	
+	private List elementList;
+	private Set elementSet;
+	private Map elementMap;
+	private Properties elementProperties;
+	
+	public List getElementList() {
+		return elementList;
+	}
+	
+	public void setElementList(List elementList) {
+		this.elementList = elementList;
+	}
+	public Set getElementSet() {
+		return elementSet;
+	}
+	
+	public void setElementSet(Set elementSet) {
+		this.elementSet = elementSet;
+	}
+	
+	public Map getElementMap() {
+		return elementMap;
+	}
+	
+	public void setElementMap(Map elementMap) {
+		this.elementMap = elementMap;
+	}
+	
+	public Properties getElementProperties() {
+		return elementProperties;
+	}
+	
+	public void setElementProperties(Properties elementProperties) {
+		this.elementProperties = elementProperties;
+	}
+	
+	
+	
+
+}
